977b97b07d206f978f82429e3b8ee0437e270f3f
huangqimin001 <huangqimin001@ke.com>
2020-10-28 02:50:01 +0800

    :art: Add todo

--- django_excel_base/excel.py
+++ django_excel_base/excel.py
@@ -132,6 +132,7 @@ def as_row_merge_xls(self):
             # Default Width: https://github.com/python-excel/xlwt/blob/master/xlwt/Column.py#L14
             # self.width = 0x0B92
             if self.auto_adjust_width:
+                # TODO: Calc width when row merge
                 width = screen.calc_width(value) * 256 if isinstance(value, basestring) else screen.calc_width(str(value)) * 256
                 if width > widths.get(colx, 0):
                     width = min(width, self.EXCEL_MAXIMUM_ALLOWED_COLUMN_WIDTH)
